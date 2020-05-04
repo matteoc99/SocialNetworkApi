06b510796e864a3d461e951e892724e8101e4516
matteo <matteo.cosi@live.it>
2020-05-04 11:57:58 +0200

    cleanup of web only elements

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -44,7 +44,8 @@ class RouteServiceProvider extends ServiceProvider
     {
         $this->mapApiRoutes();
 
-        $this->mapWebRoutes();
+        //no web routes
+       // $this->mapWebRoutes();
 
         //
     }
@@ -72,8 +73,7 @@ class RouteServiceProvider extends ServiceProvider
      */
     protected function mapApiRoutes()
     {
-        Route::prefix('api')
-            ->middleware('api')
+        Route::middleware('api')
             ->namespace($this->namespace)
             ->group(base_path('routes/api.php'));
     }
