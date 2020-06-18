74fa95cc3aa59b0630e3a04b526b9c7d09e1ffdf
matteo <matteo.cosi@live.it>
2020-06-18 18:25:47 +0200

    updated apidoc

--- app/Http/Controllers/NotificationController.php
+++ app/Http/Controllers/NotificationController.php
@@ -15,6 +15,7 @@ use Illuminate\Http\Request;
 class NotificationController extends Controller
 {
     /**
+     * Get Notifications
      * Retrives all notifications of the user
      * @return array
      */
