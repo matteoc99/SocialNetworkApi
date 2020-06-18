5d626c9fa47a95188e273fb265d8a00bedc38bd1
matteo <matteo.cosi@live.it>
2020-06-18 17:09:10 +0200

    small reset fixes

--- app/Notifications/PasswordResetRequest.php
+++ app/Notifications/PasswordResetRequest.php
@@ -43,7 +43,7 @@ class PasswordResetRequest extends Notification
     public function toMail($notifiable)
     {
 
-        $url = url('https://retronet.social/reset/?token='.$this->token);
+        $url = url('https://retronet.social/reset?token='.$this->token);
 
         return (new MailMessage)
             ->line('You are receiving this email because we received a password reset request for your account.')
