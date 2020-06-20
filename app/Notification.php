c442abc6e9eed07010ef5bcbf045271f49f60cb3
Mattia98 <io.mattia98@gmail.com>
2020-06-20 16:06:26 +0200

    first implementation for push notifications

--- app/Notification.php
+++ app/Notification.php
@@ -3,6 +3,8 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Minishlink\WebPush\WebPush;
+use Minishlink\WebPush\Subscription;
 
 class Notification extends Model
 {
@@ -29,6 +31,38 @@ class Notification extends Model
 
 
     public function sendNotification(){
-        error_log($this);
+		$auth = [
+			'VAPID' => [
+				'subject' => getenv("VAPID_SUBJECT"), // can be a mailto: or your website address
+				'publicKey' => getenv("VAPID_PUBLIC_KEY"), // (recommended) uncompressed public key P-256 encoded in Base64-URL
+				'privateKey' => getenv("VAPID_PRIVATE_KEY"), // (recommended) in fact the secret multiplier of the private key encoded in Base64-URL
+			]
+		];
+		
+		$webPush = new WebPush($auth);
+		$webPush->setAutomaticPadding(0);
+		$subscriptions = $this->authUser()->notificationEndpoints();
+		//$icon = User::where('id', $this->from_id)->get()->first()->profilePicture;
+		
+		foreach($subscriptions as $sub) {
+			$webPush->sendNotification(
+				Subscription::create(
+					json_decode($sub->endpoint, true)
+				),
+				'{"type": '.$this->type.', "text": "'.$this->payload.'", "title": "'.$this->title.'", "icon": "speck"}'
+				//"{'type': {$this->type}, 'text': '{$this->payload}', 'title': '{.$this->title}', 'icon': '{$icon}'}"
+			);
+		}
+		
+		foreach ($webPush->flush() as $report) {
+			$endpoint = $report->getRequest()->getUri()->__toString();
+
+			if ($report->isSuccess()) {
+				Log::info("[v] Message sent successfully for subscription {$endpoint}.");
+			} else {
+				Log::error("[x] Message failed to sent for subscription {$endpoint}: {$report->getReason()}");
+			}
+		}
+		
     }
 }
